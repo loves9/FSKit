02289a55038d45b4c9a070bcbf5215823c7d5135
loves9 <ifengfree@gmail.com>
2018-09-19 17:32:54 +0800

    [opt]实时链接动态库

---
+++ FenngKit/FenngKit.framework/Headers/FenngKit.h
@@ -0,0 +1,23 @@
+//
+//  FenngKit.h
+//  FenngKit
+//
+//  Created by iFeng on 2018/9/19.
+//  Copyright © 2018 iFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+//! Project version number for FenngKit.
+FOUNDATION_EXPORT double FenngKitVersionNumber = 100;
+
+//! Project version string for FenngKit.
+FOUNDATION_EXPORT const unsigned char FenngKitVersionString[] = "Bata";
+
+// In this header, you should import all the public headers of your framework using statements like #import <FenngKit/PublicHeader.h>
+
+#import "FSTableViewController.h"
+
+
+
